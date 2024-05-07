8c6f36271428e892acc4c01d9cd541c03b863c27
kunal5556 <169072606+kunal5556@users.noreply.github.com>
2024-05-07 12:53:24 +0530

    C++ solutions of all questions of assignment 1

---
+++ C++ assignment 1 solutions.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //question 1
+    cout<<"Physics\nWallah";
+    
+    //question 2
+    cout<<5+5;
+    
+    /*
+    
+    question 3
+    answer ->int takes 4 bytes
+             bool takes 1 bit
+             float takes 4 bytes
+
+    question 4
+    output ->6 4  
+
+    */
+
+    //question 5
+    cout<<"circumference of a circle with radius 10 is "<<2 * 3.14 * 10;
+    
+    /*
+    
+    question 6
+    answer
+    only FLOAT and _FLOAT can be variable names
+
+    */    
+}
